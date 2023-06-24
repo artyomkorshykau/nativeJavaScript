b4fcc18f1630764bbe1023469f7fa83993d347df
Artyom Korshykau <butovichjunior@gmail.com>
2023-06-24 21:00:09 +0300

    24.06.23

---
+++ main.js
@@ -0,0 +1,13 @@
+// const arr = [34, -345, -1, 100]
+// console.log(Math.min(...arr))
+
+const sum = (num) => {
+  let res = 0;
+  for (let i = 1; i <= num; i++) {
+    res += i
+  }
+  return res
+}
+console.log(sum(8))
+
+
